f713d9f749e4113cbe949f14595ec42be0fb27e7
08-06-a-05 <nab.txt@mail.ru>
2022-04-16 18:06:31 +0300

    bd_loader added

---
+++ bd_loader.py
@@ -0,0 +1,12 @@
+from app.renderer.models import Something
+
+file_name = "" # some file
+
+f=open(file_name,'r')
+
+
+s=input # some data
+
+while s:
+    b=Something(name=first_entity,smth=second_entity)
+    b.save()
