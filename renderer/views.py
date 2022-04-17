fac55643861b4a3c999a61af364de449f8146d5b
tsogp <tsogp@d69+Kg6dQ1VX4zdyiaHaTP4GgUM/+v17f4qLHQhmARQ>
2022-04-17 13:13:42 +0300

    plot v1

--- renderer/views.py
+++ renderer/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from plotly.offline import plot
+import plotly.graph_objects as go
+
 from .forms import AnomalyForm  
 from .models import Detector, Anomaly, DetectAnomaly
+
 def index_page(request):
     if request.method=="POST":
         if 'dec_id' in request.POST:
@@ -17,7 +21,56 @@ def index_page(request):
                 dec_an = DetectAnomaly(detector_id = dec_per,anomaly_id=an_per,rate=float(request.POST['an_rate']))
             except:
                 print('no_detector')
-    context={}
-    return render(request, 'index.html',context)
+    
+    context = {}
+
+    x = [i for i in range(0, 40 + 1)]
+    y = [i for i in range(0, 30 + 1)]
+
+    # List of graph objects for figure.
+    # Each object will contain on series of data.
+    graphs = []
+
+    # Adding linear plot of y1 vs. x.
+    graphs.append(
+        go.Scatter(x=x, y=y)
+    )
+
+    # Setting layout of the figure.
+    layout = {
+        'title': 'Карта',
+        'xaxis': {
+            'side': 'top'
+        },
+        'yaxis': {
+            'autorange': 'reversed'
+        },
+        'xaxis_title': 'X',
+        'yaxis_title': 'Y',
+        'images': [{
+            'source': '/static/img/map.png',
+            'xref': 'x',
+            'yref': 'y',
+            'x': 0,
+            'y': 0,
+            'sizex': 40,
+            'sizey': 30,
+            'sizing': 'stretch',
+            'opacity': 1,
+            'layer': 'below'
+        }],
+        'height': 1500,
+        'width': 2000,
+    }
+
+    # Getting HTML needed to render the plot.
+    plot_div = plot({
+        'data': graphs, 
+        'layout': layout,
+    }, output_type='div')
+    
+    context['plot_div'] = plot_div
+
+    return render(request, 'index.html', context)
 
     
