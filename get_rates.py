96e29eb7e1b9ee6fedd953c5383996b19eeb133e
Lev Yuvensky <levayu22@gmail.com>
2022-04-17 12:39:38 +0300

    Change get_rates.py

--- get_rates.py
+++ get_rates.py
@@ -3,13 +3,14 @@ import json
 FILE_NAME = "coords.json"
 
 
-def get_swans_rates(file_name):
+def get_swans_true_false(file_name):
     with open(file_name, "r") as read_file:
         database = json.load(read_file)
 
         swans = dict()
 
         for detector in database["message"]:
+            """
             for swan in detector["swans"]:
                 swan_id = swan["id"]
                 swan_rate = swan["rate"]
@@ -18,8 +19,48 @@ def get_swans_rates(file_name):
                     swans[swan_id] = []
 
                 swans[swan_id].append(swan_rate)
+            """
+            detector_swans_ids = []
 
+            for swan in detector["swans"]:
+                swan_id = swan["id"]
+                swan_rate = swan["rate"]
+
+                detector_swans_ids.append(swan_id)
+
+                if swan_id not in swans.keys():
+                    swans[swan_id] = []
+                swans[swan_id].append(True)
+
+            for s in swans.keys():
+                if s not in detector_swans_ids:
+                    swans[s].append(False)
         return swans
 
+def get_swans_rates(file_name):
+    with open(file_name, "r") as read_file:
+        database = json.load(read_file)
+
+        swans = dict()
+
+        for detector in database["message"]:
+            detector_swans_ids = []
+
+            for swan in detector["swans"]:
+                swan_id = swan["id"]
+                swan_rate = swan["rate"]
+
+                detector_swans_ids.append(swan_id)
+
+                if swan_id not in swans.keys():
+                    swans[swan_id] = []
+                swans[swan_id].append(swan_rate)
+
+            for s in swans.keys():
+                if s not in detector_swans_ids:
+                    swans[s].append(0.0)
+        return swans
+
+print(get_swans_true_false(FILE_NAME))
 
 print(get_swans_rates(FILE_NAME))
