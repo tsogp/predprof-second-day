50cb0f5191586433a115991100a11998936a1329
tsogp <tsogp@d69+Kg6dQ1VX4zdyiaHaTP4GgUM/+v17f4qLHQhmARQ>
2022-04-17 11:17:54 +0300

    db changes

--- renderer/models.py
+++ renderer/models.py
@@ -1,18 +1,27 @@
 from datetime import date
 from django.db import models
 
-class Anomaly(models.Model):
-    name = models.CharField(max_length=200)
-    rate = models.FloatField(blank=False)
-
-    def __str__(self):
-        return self.name
-
 class Detector(models.Model):
     x_coord = models.FloatField(blank=False)
     y_coord = models.FloatField(blank=False)
-    anomaly_id = models.ForeignKey(Anomaly, on_delete=models.CASCADE)
-    
+
     def __str__(self):
         return self.pk
+
+class Anomaly(models.Model):
+    center_x = models.FloatField()
+    center_y = models.FloatField()
+
+    def __str__(self):
+        return self.pk
+
+class DetectAnomaly(models.Model):
+    detector_id = models.ForeignKey(Detector, on_delete=models.CASCADE)
+    anomaly_id = models.ForeignKey(Anomaly, on_delete=models.CASCADE)
+    rate = models.FloatField(blank=False)   
+
+    def __str__(self):
+        return self.pk
+
+
 
