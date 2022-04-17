35184466ad8f74fad4e7f7ebc9ec9997ef65bfa4
tsogp <tsogp@d69+Kg6dQ1VX4zdyiaHaTP4GgUM/+v17f4qLHQhmARQ>
2022-04-17 15:35:10 +0300

    bruh...

--- renderer/views.py
+++ renderer/views.py
@@ -6,85 +6,70 @@ import plotly.graph_objects as go
 from .forms import AnomalyForm  
 from .models import Detector, Anomaly, DetectAnomaly
 
-def find_way():
-    SIZE = 6
-    matrix = [[random.randint(1, 2)] * SIZE for i in range(SIZE)]
-    # matrix = [[1] * SIZE for i in range(SIZE)]
-    for i in range(SIZE):
-        matrix[0][i] = 0
-        matrix[i][0] = 0
+SIZE = 10
+def f(matrix, q, finish, point, current_way, current_value, been):
+    # print(current_way)
+    if point == finish:
+        q.append((current_value, current_way))
+        return
 
-    for r in matrix:
-        print(r)
+    if point[1] - 1 > 0 and not been[point[1] - 1][point[0]] and 0 < matrix[point[1] - 1][point[0]] < 2:
+        new_point = (point[0], point[1] - 1)
+        been[new_point[1]][new_point[0]] = True
+        f(matrix, q, finish, new_point, current_way + [new_point], current_value + matrix[new_point[1]][new_point[0]], been)
+        been[new_point[1]][new_point[0]] = False
 
-    start = (1, 1)
-    finish = (3, 3)
+    if point[0] - 1 > 0 and not been[point[1]][point[0] - 1] and 0 < matrix[point[1]][point[0] - 1] < 2:
+        new_point = (point[0] - 1, point[1])
+        been[new_point[1]][new_point[0]] = True
+        f(matrix, q, finish, new_point, current_way + [new_point], current_value + matrix[new_point[1]][new_point[0]], been)
+        been[new_point[1]][new_point[0]] = False
 
+    if point[0] + 1 < SIZE and not been[point[1]][point[0] + 1] and 0 < matrix[point[1]][point[0] + 1] < 2:
+        new_point = (point[0] + 1, point[1])
+        been[new_point[1]][new_point[0]] = True
+        f(matrix, q, finish, new_point, current_way + [new_point], current_value + matrix[new_point[1]][new_point[0]], been)
+        been[new_point[1]][new_point[0]] = False
 
-    def f(q, finish, point, current_way, current_value, been):
-        # print(current_way)
-        if point == finish:
-            q.append((current_value, current_way))
-            return
-
-        if point[1] - 1 > 0 and not been[point[1] - 1][point[0]] and 0 < matrix[point[1] - 1][point[0]] < 2:
-            new_point = (point[0], point[1] - 1)
-            been[new_point[1]][new_point[0]] = True
-            f(q, finish, new_point, current_way + [new_point], current_value + matrix[new_point[1]][new_point[0]], been)
-            been[new_point[1]][new_point[0]] = False
-
-        if point[0] - 1 > 0 and not been[point[1]][point[0] - 1] and 0 < matrix[point[1]][point[0] - 1] < 2:
-            new_point = (point[0] - 1, point[1])
-            been[new_point[1]][new_point[0]] = True
-            f(q, finish, new_point, current_way + [new_point], current_value + matrix[new_point[1]][new_point[0]], been)
-            been[new_point[1]][new_point[0]] = False
-
-        if point[0] + 1 < SIZE and not been[point[1]][point[0] + 1] and 0 < matrix[point[1]][point[0] + 1] < 2:
-            new_point = (point[0] + 1, point[1])
-            been[new_point[1]][new_point[0]] = True
-            f(q, finish, new_point, current_way + [new_point], current_value + matrix[new_point[1]][new_point[0]], been)
-            been[new_point[1]][new_point[0]] = False
-
-        if point[1] + 1 < SIZE and not been[point[1] + 1][point[0]] and 0 < matrix[point[1] + 1][point[0]] < 2:
-            new_point = (point[0], point[1] + 1)
-            been[new_point[1]][new_point[0]] = True
-            f(q, finish, new_point, current_way + [new_point], current_value + matrix[new_point[1]][new_point[0]], been)
-            been[new_point[1]][new_point[0]] = False
+    if point[1] + 1 < SIZE and not been[point[1] + 1][point[0]] and 0 < matrix[point[1] + 1][point[0]] < 2:
+        new_point = (point[0], point[1] + 1)
+        been[new_point[1]][new_point[0]] = True
+        f(matrix, q, finish, new_point, current_way + [new_point], current_value + matrix[new_point[1]][new_point[0]], been)
+        been[new_point[1]][new_point[0]] = False
 
 
-    def find_way(matrix, start, finish):
-        been = [[False] * SIZE for j in range(SIZE)]
-        been[start[1]][start[0]] = True
+def find_way(matrix, start, finish):
+    been = [[False] * SIZE for j in range(SIZE)]
+    been[start[1]][start[0]] = True
 
-        q = []
+    q = []
 
-        f(q, finish, start, [start], matrix[start[1]][start[0]], been)
-        # print(q)
+    f(matrix, q, finish, start, [start], matrix[start[1]][start[0]], been)
+    # print(q)
 
-        min_d = 999999
-        ways = []
-        for a in q:
-            if a[0] < min_d:
-                min_d = a[0]
-                ways = [a[1]]
-            elif a[0] == min_d:
-                ways.append(a[1])
+    min_d = 999999
+    ways = []
+    for a in q:
+        if a[0] < min_d:
+            min_d = a[0]
+            ways = [a[1]]
+        elif a[0] == min_d:
+            ways.append(a[1])
 
-        min_len = 999999
-        final_ways = []
-        for w in ways:
-            if len(w) < min_len:
-                min_len = len(w)
-                final_ways = [w]
-            elif len(w) == min_len:
-                final_ways.append(w)
+    min_len = 999999
+    final_ways = []
+    for w in ways:
+        if len(w) < min_len:
+            min_len = len(w)
+            final_ways = [w]
+        elif len(w) == min_len:
+            final_ways.append(w)
 
-        if len(final_ways) > 0:
-            return final_ways[0]
+    if len(final_ways) > 0:
+        return final_ways[0]
 
-        return []
+    return []
 
-    return find_way(matrix,start,finish)
     # print(find_way(matrix, start, finish))
 
 
@@ -171,6 +156,8 @@ def find_detectors():
 
 def index_page(request):
     spis = []
+    spis_x = []
+
     # print(find_detectors())
     find_detectors()
     if request.method=="POST":
@@ -199,6 +186,7 @@ def index_page(request):
             finish_x+=1
             finish_y+=1'''
             spis = find_way(make_matrice(find_detectors()),(start_x,start_y),(finish_x,finish_y))
+            print(spis)
             spis_x=[]
             spis_y=[]
             for abra in spis:
@@ -219,17 +207,18 @@ def index_page(request):
         anomalies_x.append(anomalies[i].center_x)
         anomalies_y.append(anomalies[i].center_y)
         anomalies_name.append(anomalies[i].id)
-        anomalies_rate.append(anomalies[i].real_rate * 7)
+        anomalies_rate.append(anomalies[i].real_rate * 5)
         anomalies_rate_text.append('rate: ' + str(anomalies[i].real_rate))
 
     x = [i for i in range(0, 40 + 1)]
     y = [i for i in range(0, 40 + 1)]
 
     graphs = []
-
-    '''graphs.append(
-        go.Scatter(x=spis_x, y=spis_y)
-    )'''
+    print(spis_x)
+    if spis_x:
+        graphs.append(
+            go.Scatter(x=spis_x, y=spis_y)
+        )
 
     graphs.append(
         go.Scatter(
@@ -244,9 +233,7 @@ def index_page(request):
         )
     )
     
-    graphs.append(
-        go.Scatter(x=x, y=y)
-    )
+    
 
     layout = {
         'title': 'Карта',
