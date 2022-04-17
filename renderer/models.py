6a8c1370f771bae440ab5d90f7c4a909ee51a825
tsogp <tsogp@d69+Kg6dQ1VX4zdyiaHaTP4GgUM/+v17f4qLHQhmARQ>
2022-04-17 10:57:47 +0300

    migrations

--- renderer/models.py
+++ renderer/models.py
@@ -1,31 +1,18 @@
 from datetime import date
-
 from django.db import models
 
-class Blog(models.Model):
-    name = models.CharField(max_length=100)
-    tagline = models.TextField()
-
-    def __str__(self):
-        return self.name
-
-class Author(models.Model):
+class Anomaly(models.Model):
     name = models.CharField(max_length=200)
-    email = models.EmailField()
+    rate = models.FloatField(blank=False)
 
     def __str__(self):
         return self.name
 
-class Entry(models.Model):
-    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
-    headline = models.CharField(max_length=255)
-    body_text = models.TextField()
-    pub_date = models.DateField()
-    mod_date = models.DateField(default=date.today)
-    authors = models.ManyToManyField(Author)
-    number_of_comments = models.IntegerField(default=0)
-    number_of_pingbacks = models.IntegerField(default=0)
-    rating = models.IntegerField(default=5)
-
+class Detector(models.Model):
+    x_coord = models.FloatField(blank=False)
+    y_coord = models.FloatField(blank=False)
+    anomaly_id = models.ForeignKey(Anomaly, on_delete=models.CASCADE)
+    
     def __str__(self):
-        return self.headline
+        return self.pk
+
