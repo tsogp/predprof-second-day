f79738dba2f22da185a2a178e2ffdbb13d1233c2
ArsPro13 <Vintbolt13@gmail.com>
2022-04-17 12:01:03 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,29 +1,29 @@
 x = [9, 33, 9, 32]
 y = [9, 9, 23, 22]
-F = [[0.1626, 0.2667, 0.262, 0.9231]]
+F = {'5b6eb38b': [0.1626, 0.2667, 0.262, 0.9231], 'c00b92ba': [0.249, 0.6186, 0.1846, 0.4138], '931ede7b': [1.4634, 0.1412, 0.1592], '661c2e66': [0.1859, 0.2262, 0.2703, 0.4587], 'c2958c11': [0.2006, 1.1475, 0.1022, 0.2006], '08d55d4f': [0.2602, 0.1918, 0.6931, 0.4142]}
 
 def rasst(x, y):
     return((x**2 + y**2))
 
-for detector in range(1):
-    delta=0.1
+for detector in F.keys():
+    delta=1
     numsx=[]
     numsy=[]
-    while (len(numsx)!=1):
+    while (len(numsx)==0 or len(numsx)>2):
         numsx=[]
         numsy=[]
         delta*=0.8
         for x0 in range(1, 36):
             for y0 in range(1, 36):
                 flag=True
-                for f1 in range(4):
-                    for f2 in range(f1+1, 4):
+                for f1 in range(len(F[detector])):
+                    for f2 in range(f1+1, len(F[detector])):
                         r1=F[detector][f1]*rasst(x0-x[f1], y0-y[f1])
                         r2=F[detector][f2]*rasst(x0-x[f2], y0-y[f2])
                         if (abs(r2-r1)<=delta):
                             numsx.append(x0)
                             numsy.append(y0)
-    print(detector, ":", *numsx, *numsy)
+    print(detector, ":", numsx[0], numsy[0])
 
                         
 
